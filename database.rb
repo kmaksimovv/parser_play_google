305fcf2efa908ee9fcb0ccc85a29a3983f710ced
Maksimov Konstantin <kmaksimovv@gmail.com>
2019-07-06 21:01:32 +0400

    correct readme file

--- database.rb
+++ database.rb
@@ -1,13 +1,21 @@
 require 'pg'
 
+# your`s settings
+pg_host = 'localhost'
+pg_user = 'postgres'
+pg_passwd = 'postgres'
+
+
 begin
-  con = PG::Connection.new(host: 'localhost', user: 'postgres', password: 'postgres')
+  con = PG::Connection.new(host: pg_host, user: pg_user, password: pg_passwd)
   puts 'Successfully created connection to database'
+
   con.exec('CREATE DATABASE apps')
   puts 'Successfully created database'
-  conn = PG::Connection.new(host: 'localhost', dbname: 'apps', user: 'postgres', password: 'postgres')
+
+  conn = PG::Connection.new(host: pg_host, dbname: 'apps', user: pg_user, password: pg_passwd)
   puts 'Successfully connected database apps'
-  conn.exec("CREATE TABLE app_info(id SERIAL, name VARCHAR, ref_icon VARCHAR, category VARCHAR, 
+  conn.exec("CREATE TABLE app_info(id SERIAL, name VARCHAR, ref_icon VARCHAR, category VARCHAR,
              rating VARCHAR,dowloads VARCHAR, author VARCHAR, author_email VARCHAR, created_at TIMESTAMPTZ)")
   puts 'Successfully created table apps_info'
 rescue PG::Error => e
--- main.rb
+++ main.rb
@@ -1,6 +1,10 @@
 require './lib/google_play_service'
 require 'pg'
 
+pg_host = 'localhost'
+pg_user = 'postgres'
+pg_passwd = 'postgres'
+
 # test app 'https://play.google.com/store/apps/details?id=com.idamob.tinkoff.android'
 
 link_app = ARGV
@@ -15,10 +19,11 @@ puts "name: #{gps.name},
       author: #{gps.author},
       author_email: #{gps.author_email}"
 begin
-  con = PG::Connection.new(host: 'localhost', dbname: 'apps', user: 'postgres', password: 'postgres')
-  sql = "INSERT INTO app_info (name, ref_icon, category, rating, dowloads, author, author_email, created_at) VALUES('#{gps.name}', 
+  con = PG::Connection.new(host: pg_host, dbname: 'apps', user: pg_user, password: pg_passwd)
+  sql = "INSERT INTO app_info (name, ref_icon, category, rating, dowloads, author, author_email, created_at) VALUES('#{gps.name}',
          '#{gps.ref_icon}', '#{gps.category}', '#{gps.rating}', '#{gps.dowloads}', '#{gps.author}', '#{gps.author_email}', 'now()')"
   con.exec(sql)
+  puts 'Successfully created info for app'
 rescue PG::Error => e
   puts e.message
 ensure
